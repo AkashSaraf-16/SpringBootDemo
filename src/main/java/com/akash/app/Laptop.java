706e3475aadbafdc6ddc92aa92981db4bbc76e6a
Akash Saraf <akashsaraf24@gmail.com>
2025-09-02 09:23:24 +0530

    Autowiring

---
+++ src/main/java/com/akash/app/Laptop.java
@@ -0,0 +1,10 @@
+package com.akash.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling...");
+    }
+}
